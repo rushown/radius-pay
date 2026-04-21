c31856effdee2ce5ffa1eccab4613d011abcb178
Rocean <roceanadhikari@gmail.com>
2026-04-21 10:35:21 +0530

    added jeet config and remapping

---
+++ backend/jeet.config.js
@@ -0,0 +1,7 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.js'],
+  collectCoverageFrom: ['src/**/*.js', '!src/app.js'],
+  coverageThreshold: { global: { lines: 70 } },
+};
